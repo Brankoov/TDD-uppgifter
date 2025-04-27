988438044bd738c734f621f5254265d623c6f3c6
Branko <banne_nikolic@hotmail.com>
2025-04-27 18:50:23 +0200

    Fix: Gjort små ändringar tidigare

--- src/main/java/se/branko/CalculatorService.java
+++ src/main/java/se/branko/CalculatorService.java
@@ -1,4 +1,15 @@
 package se.branko;
 
 public class CalculatorService {
+
+    Calculator calculator = new Calculator();
+
+
+
+    public double safeDivide(double a, double b) {
+        if( b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return calculator.divide(a, b);
+    }
 }
--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -1,2 +1,68 @@
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import se.branko.Calculator;
+import se.branko.CalculatorService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
 public class CalculatorServiceTest {
+
+
+    @Test
+    public void additionTest() {
+
+        Calculator calculator = new Calculator();
+
+        double result = calculator.add(2,2);
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void subtractionTest() {
+        Calculator calculator = new Calculator();
+
+        double result = calculator.sub(5,2);
+        assertEquals(3, result);
+    }
+
+    @Test
+    @DisplayName("Multiplying two numbers and show/get result")
+    public void multiplicationTest() {
+
+        Calculator calculator = new Calculator();
+
+        double result = calculator.multi(5,5);
+        assertEquals(25, result);
+    }
+
+    @Test
+    @DisplayName("Dividing two numbers and get the result")
+    public void divisionTest() {
+        Calculator calculator = new Calculator();
+
+        double result = calculator.divide(10,2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    @DisplayName("Dividing by 0 and throw ArithmeticException")
+    public void safeDivisionTest() {
+        CalculatorService calculatorService = new CalculatorService();
+
+        assertThrows(ArithmeticException.class, () ->{
+           calculatorService.safeDivide(2,0);
+        });
+    }
+
+    @Test
+    @DisplayName("Adding two decimal numbers to cause round up error and then handling it with delta")
+    public void floatingPointAddition() {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(0.1, 0.2);
+        assertEquals(0.3, result, 0.00001); //Måste tillåta en lite skillnad (delta) vanligt med float, avrundningsfel
+    }
+
 }
